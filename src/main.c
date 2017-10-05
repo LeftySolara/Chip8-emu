81c457dbf6ed8fab4f8c703348799aefb71d81c3
Jalen Adams <leftysolara@gmail.com>
2017-10-05 12:39:15 -0400

    alert and exit if no program specified

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
 #include "Chip8Config.h"
 #include "chip8.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char **argv)
 {
     printf("Chip8-emu version %d.%d\n%s\n\n",
            Chip8_VERSION_MAJOR, Chip8_VERSION_MINOR, Chip8_COPYRIGHT_LINE);
+
+    if (argc < 2) {
+        printf("No Chip-8 program specified. Exiting...\n");
+        exit(1);
+    }
 
     const char *prog = argv[1];
     struct Chip8 *chip8 = chip8_init();
