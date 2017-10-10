c0b2a3b397aeaa51e91f163b0b7e98a81fb4129a
Jalen Adams <leftysolara@gmail.com>
2017-10-09 20:30:32 -0400

    add SDL2 library

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SDL2/SDL.h>
 
 int main (int argc, char **argv)
 {
@@ -13,6 +14,29 @@ int main (int argc, char **argv)
         printf("No Chip-8 program specified. Exiting...\n");
         exit(1);
     }
+
+    /* Sample SDL stuff to test that the library is working properly */
+    SDL_Window *window = NULL;
+    SDL_Surface *surface = NULL;
+
+    const int screen_width = 640;
+    const int screen_height = 480;
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+        printf("Could not initialize SDL. Error: %s\n", SDL_GetError());
+    else {
+        window = SDL_CreateWindow("Chip8-emu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+            printf("Could not create window. Error: %s\n", SDL_GetError());
+        else {
+            surface = SDL_GetWindowSurface(window);
+            SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+            SDL_Delay(5000);
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     srand(time(NULL));
     const char *prog = argv[1];
